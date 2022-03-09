41f0dc4c7c7fca8630d8c135eacbcbc9fa1858f9
Laura190 <l.cooper.5@warwick.ac.uk>
2022-03-09 16:30:44 +0000

    replace example import script

--- Examples/MicroscopeImport.sh
+++ Examples/MicroscopeImport.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-##Import for individual Windows computer
-# Parameters:
-# Name of microscope
-micro=laura
-# Location where data is stored
-path=/mnt/d/Data
-# Location of Imported folder
-imp_path=$path/Imported
-# Location where log files will be stored
-archive=$path/ImportLogs
-# Location of password file
-pass_file=/home/user/.pass.txt
-# Path to OMERO environment
-env=/home/user/omero-env
-# Path to configuration file
-conf_file=/home/user/import.cfg
-
-#Moves old data
-bash OMEROimport move_old --location="$path/"
+## Import for individua Windows computer
+#Read config file
+config=$( bash OMEROimport check_config )
+if [ "${config//ERROR/}" != "${config}" ]; then
+      echo "${config}"
+      exit 1
+else
+  source $config
+fi
 
 #Import data for each folder on path
 log_files=()
@@ -27,9 +17,10 @@ for folder in "$path"/*; do
   if [ $(basename "$folder") != "Imported" ] && [ $(basename "$folder") != "OldData" ] && [ $(basename "$folder") != "imported" ]; then
     user=$(bash OMEROimport read_file --file="$folder"/.username.txt)
     if [[ $user != ERROR* ]]; then
-      result=$(bash OMEROimport import_folder --password_file="$pass_file" \
-                 --environment="$env" --user="$user" --folder="$folder" \
-                 --config="$conf_file") 
+      # Move old data
+      bash OMEROimport move_old --location="$folder"
+      # Import data
+      result=$(bash OMEROimport import_folder --user="$user" --folder="$folder")
     else
       result=$user
     fi
@@ -37,6 +28,7 @@ for folder in "$path"/*; do
        [ "${result//ERROR/}" != "${result}" ]; then
       echo "${result}"
     else
+      bash OMEROimport move_imports --logfile="${result}" --target="$imp_path"
       log_files[$i]="$result"
       i=$((i+1))
     fi
@@ -45,7 +37,6 @@ done
 
 # Move imported files to imp_path
 for log in ${log_files[@]}; do
-  bash OMEROimport move_imports --logfile="$log" --target="$imp_path"
   mv "$log" "$archive"/"$micro"_$(date +%Y%m%d)_"$log"
   rm "Before_$log"
 done
